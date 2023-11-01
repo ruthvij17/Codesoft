0bd9b8f8dbdb927c52acd09aa5c5ac1dc1373a87
ruthvij17 <130194046+ruthvij17@users.noreply.github.com>
2023-11-01 12:27:54 +0530

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,12 +1,8 @@
 import tkinter as tk
-
-# Function to update the display
 def button_click(number):
     current = entry.get()
     entry.delete(0, tk.END)
     entry.insert(0, current + str(number))
-
-# Function to perform calculations
 def calculate():
     try:
         result = eval(entry.get())
@@ -15,24 +11,16 @@ def calculate():
     except:
         entry.delete(0, tk.END)
         entry.insert(0, "Error")
-
-# Create the main window
 window = tk.Tk()
 window.title("Simple Calculator")
-
-# Create an entry widget for the display
 entry = tk.Entry(window, width=20)
 entry.grid(row=0, column=0, columnspan=4)
-
-# Define buttons
 buttons = [
     '7', '8', '9', '/',
     '4', '5', '6', '*',
     '1', '2', '3', '-',
     '0', 'C', '=', '+'
 ]
-
-# Create and place the buttons
 row, col = 1, 0
 for button in buttons:
     if button == '=':
@@ -45,6 +33,4 @@ for button in buttons:
     if col > 3:
         col = 0
         row += 1
-
-# Start the main loop
 window.mainloop()
