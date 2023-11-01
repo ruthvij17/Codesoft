36ac902a86ed0649b54b4c374777e1eb8822d2ee
ruthvij17 <130194046+ruthvij17@users.noreply.github.com>
2023-11-01 12:28:58 +0530

    random_password_gen.py

--- random_password_gen.py
+++ random_password_gen.py
@@ -1,22 +1,13 @@
 import random
 import math
-
 alpha = "abcdefghijklmnopqrstuvwxyz"
 num = "0123456789"
 special = "@#$%&*"
-
-# pass_len=random.randint(8,13)  #without User INput
 pass_len = int(input("Enter Password Length"))
-
-# length of password by 50-30-20 formula
 alpha_len = pass_len//2
 num_len = math.ceil(pass_len*30/100)
 special_len = pass_len-(alpha_len+num_len)
-
-
 password = []
-
-
 def generate_pass(length, array, is_alpha=False):
     for i in range(length):
         index = random.randint(0, len(array) - 1)
@@ -28,11 +19,11 @@ def generate_pass(length, array, is_alpha=False):
         password.append(character)
 
 
-# alpha password
+# alpha
 generate_pass(alpha_len, alpha, True)
-# numeric password
+# numeric
 generate_pass(num_len, num)
-# special Character password
+# special Character 
 generate_pass(special_len, special)
 # suffle the generated password list
 random.shuffle(password)
